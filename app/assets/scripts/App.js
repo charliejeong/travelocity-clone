271303dbf28742cfd4f42a2db547899128c5a935
Charlie <jeehoon.jeong.711@gmail.com>
2019-12-08 21:34:56 -0600

    completed modals

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,17 +2,31 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu.js'
 import RevealOnScroll from './modules/RevealOnScroll.js'
 import StickyHeader from './modules/StickyHeader.js'
-import Modal from './modules/Modal.js'
 
-
-new Modal();
-let stickyHeader = new StickyHeader();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+new MobileMenu();
+let modal
+
+
+document.querySelectorAll(".open-modal").forEach(el => {
+	el.addEventListener("click", e =>{
+		e.preventDefault()
+		if (typeof modal == "undefined") {
+			import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+				modal = new x.default()
+				setTimeout( () => modal.openTheModal(), 20)
+			}).catch(() => console.log("there was a problem"))
+		} else {
+			modal.openTheModal()
+		}
+	})
+})
+
 
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
 	module.hot.accept()
 }
-
